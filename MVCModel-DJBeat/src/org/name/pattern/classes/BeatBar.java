c2d4529f04c6ecb46e53351221744bef33cabf2c
Adonis <adonis.settouf@gmail.com>
2015-11-25 17:46:05 +0100

    Finalizing the beatbar for multi threading

--- MVCModel-DJBeat/src/org/name/pattern/classes/BeatBar.java
+++ MVCModel-DJBeat/src/org/name/pattern/classes/BeatBar.java
@@ -5,8 +5,14 @@ import javax.swing.*;
 public class BeatBar extends JProgressBar implements Runnable { 
 	private static final long serialVersionUID = 2L;
     JProgressBar progressBar;
+	Thread thread;
 
-	
+	public BeatBar() {
+		thread = new Thread(this);
+		setMaximum(100);
+		thread.start();
+	}
+
 	public void run() {
 		for(;;) {
 			int value = getValue();
